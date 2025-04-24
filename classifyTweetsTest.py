4dc8b2393c4f20dc4fa10ac028698a8b3f1e8e3f
Varun Suryan <varun.suryan@cat.com>
2025-04-24 15:47:22 -0500

    Embeddings printed

--- classifyTweetsTest.py
+++ classifyTweetsTest.py
@@ -1,121 +1,14 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import random
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import log_loss, accuracy_score
-from sklearn.datasets import make_blobs, make_circles, make_moons
-from sklearn.inspection import DecisionBoundaryDisplay
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras.layers import TextVectorization, Embedding
+import torch
+x = torch.randn(3, 4)
 
-# embedding = Embedding(input_dim=3, output_dim=2)
+dim1 = torch.stack((x, x), dim=0)
+print(dim1)
+print(dim1.size())
 
-# model = keras.Sequential([Embedding(input_dim=5, output_dim=2, name="my_embedding")])
-# model.compile('rmsprop')
+dim2 = torch.stack((x, x), dim=1)
+print(dim2)
+print(dim2.size())
 
-# x = np.random.rand(1, 2, 3)
-# y = keras.layers.GlobalAveragePooling1D()(x)
-
-# print(x)
-# print(y)
-
-epochs = 50000
-X, y = make_blobs(n_samples=500, centers=[[-1, -1], [1, 1]], cluster_std=0.5, random_state=0)
-# X, y = make_circles(n_samples=500, noise=0.1, factor=0.3, random_state=0)
-
-# plt.scatter(X[:, 0], X[:, 1], c=y)
-# plt.title("Two normally-distributed clusters")
-# plt.show(block=True)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-y_train, y_test = y_train[:, np.newaxis], y_test[:, np.newaxis]
-
-# X_train, y_train = np.array([[-1, -1], [1, 1]]), np.array([[0], [1]])
-n_train, learning_rate = X_train.shape[0], 1e-1
-
-[w1, w2, w3, w4, w5, w6] = map(float, np.random.rand(6, 1))
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-losses_train, losses_val, accs_train, accs_val = [], [], [], []
-for epoch in range(epochs):
-    # Forward pass
-    a_11 = np.matmul(X_train, np.array([[w1],  [w3]])) + 1 # n*2 * 2*1 = n*1
-    a_12 = np.matmul(X_train, np.array([[w2],  [w4]])) + 1 # n*2 * 2*1 = n*1
-    out_11 = sigmoid(a_11) #n*1
-    out_12 = sigmoid(a_12) #n*1    
-    a = np.matmul(np.column_stack((out_11, out_12)), np.array([[w5],  [w6]])) + 1 # n*2 * 2*1
-    y_hat = sigmoid(a)
-
-    # Backward pass
-    derivative_a_11 = np.multiply(out_11, 1-out_11)
-    derivative_a_12 = np.multiply(out_12, 1-out_12)
-    diff = np.subtract(y_train, y_hat) # n*1
-    del_w6 = -np.dot(diff.T, out_12)/n_train # divide by n
-    del_w5 = -np.dot(diff.T, out_11)/n_train
-    del_w4 = -w6*np.dot(np.multiply(diff, derivative_a_12).T, X_train[:, [1]])/n_train
-    del_w3 = -w5*np.dot(np.multiply(diff, derivative_a_11).T, X_train[:, [1]])/n_train
-    del_w2 = -w6*np.dot(np.multiply(diff, derivative_a_12).T, X_train[:, [0]])/n_train
-    del_w1 = -w5*np.dot(np.multiply(diff, derivative_a_11).T, X_train[:, [0]])/n_train
-
-    w1 -= learning_rate*del_w1[0][0]
-    w2 -= learning_rate*del_w2[0][0]
-    w3 -= learning_rate*del_w3[0][0]
-    w4 -= learning_rate*del_w4[0][0]
-    w5 -= learning_rate*del_w5[0][0]
-    w6 -= learning_rate*del_w6[0][0]
-
-    if epoch % 500 == 0:
-        print("Epoch {}".format(epoch))
-        train_loss = log_loss(y_train, y_hat)
-        accuracy_train = accuracy_score(y_train, np.round(y_hat))
-        print("Training loss: {}.".format(train_loss) + " Accuracy {}.".format(accuracy_train))
-        a_11 = np.matmul(X_test, [w1,  w3]) # n*2 * 2*1
-        a_12 = np.matmul(X_test, [w2,  w4]) # n*2 * 2*1
-        out_11 = sigmoid(a_11) #n*1
-        out_12 = sigmoid(a_12) #n*1
-        a = np.matmul(np.column_stack((out_11, out_12)), [w5 , w6]) + 1 # n*2 * 2*1 
-        y_test_hat = sigmoid(a)
-        val_loss = log_loss(y_test, y_test_hat)
-        accuracy_val = accuracy_score(y_test, np.round(y_test_hat))
-        print("Validation loss: {}".format(val_loss) + " Accuracy {}.".format(accuracy_val))
-        print("--------")
-        losses_train.append(train_loss)
-        losses_val.append(val_loss)
-        accs_train.append(accuracy_train)
-        accs_val.append(accuracy_val)
-
-# plt.plot(losses_val, 'r', label = "Val loss")
-# plt.plot(losses_train, 'b', label = "Train loss")
-# plt.legend()
-# plt.grid()
-# plt.show()
-#
-# plt.plot(accs_train, 'g', label = "Train accuracy")
-# plt.plot(accs_val, 'b', label = "Val accuracy")
-# plt.legend()
-# plt.grid()
-# plt.show()
-
-min_x, min_y, max_x, max_y = -3, -3, 3, 3
-feature_1, feature_2 = np.meshgrid(np.linspace(min_x, max_x), np.linspace(min_y, max_y))
-grid = np.vstack([feature_1.ravel(), feature_2.ravel()]).T
-
-# Plot the decision surface
-a_11 = np.matmul(grid, [w1, w3])  # n*2 * 2*1
-a_12 = np.matmul(grid, [w2, w4])  # n*2 * 2*1
-out_11 = sigmoid(a_11)  # n*1
-out_12 = sigmoid(a_12)  # n*1
-a = np.matmul(np.column_stack((out_11, out_12)), [w5, w6]) + 1  # n*2 * 2*1
-y_test_hat = np.reshape(sigmoid(a), feature_1.shape)
-display = DecisionBoundaryDisplay(xx0=feature_1, xx1=feature_2, response=y_test_hat)
-display.plot()
-
-display.ax_.scatter(X[:, 0], X[:, 1], c=y, edgecolor="black")
-plt.show()
+dim3 = torch.stack((x, x), dim=2)
+print(dim3)
+print(dim3.size())
--- trainLLM.py
+++ trainLLM.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -15,7 +17,6 @@ def decode(li):
     return "".join([itos[i] for i in li])
 
 data = torch.tensor(encode(text), dtype=torch.long)
-
 train_size = 0.9
 n = int(train_size*len(data))
 
@@ -29,3 +30,36 @@ for t in range(block_size):
     context, target = x[:t+1], y[t]
     print("When context is {}, the target is {}".format(context.tolist(), target.tolist()))
 
+torch.manual_seed(1337)
+batch_size = 4
+def get_batch(dataType: str):
+    _data = train_data if dataType == "train" else val_data
+    ix = torch.randint(len(_data)-block_size, (batch_size, ))
+    x = torch.stack([_data[i:i+block_size] for i in ix])
+    y = torch.stack([_data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch("train")
+print("inputs:")
+print(xb.shape, yb.shape)
+print(xb)
+print(yb)
+
+print('-----')
+# for b in range(batch_size):
+#     for t in range(block_size):
+#         context, target = xb[b, :t+1], yb[b, t]
+#         print(f"When input is {context.tolist()}, the target is {target}")
+
+vocab_size = len(chars)
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+    def forward(self, idx, targets):
+        logits = self.token_embedding_table(idx)
+        return logits
+
+m = BigramLanguageModel(vocab_size)
+out = m(xb, yb)
+print(out)
