807a245180f98d7daf41b752cc9a99ae34c8b1c5
Varun Suryan <varun.suryan@cat.com>
2025-05-08 15:23:56 -0500

    Debugged code. Now produces reasonable output.

--- testFnLLM.py
+++ testFnLLM.py
@@ -1,17 +1,8 @@
 import torch
 from torch.nn import functional as F
 
-b, t, c = 2, 2, 8
+b, t, c = 2, 3, 4
 
 x = torch.randn(b, t, c)
 print(x)
-tril = torch.tril(torch.ones(t, t))
-print(tril)
-wei = torch.zeros((t, t))
-wei = wei.masked_fill(tril==0, -float('inf'))
-print(wei)
-wei = F.softmax(wei, dim=1)
-print(wei)
-out = wei @ x
-print(out.shape)
-print(out)
+print(x[:, -1, :])
